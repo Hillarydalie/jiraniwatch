a25456adf7a38bda24661aefad3fe2bfaa068835
hillarydalie <hidalie@gmail.com>
2019-10-30 10:45:29 +0300

    Create a virtual environment to display our dependencies.

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hillarydalie/Documents/Moringa School/Django/jiraniwatch/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
